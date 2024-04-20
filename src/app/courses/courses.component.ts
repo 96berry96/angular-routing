223f72817b413e2585d859b94e976f9e5ab12ef5
96berry96 <miltoncsedev@gmail.com>
2024-04-20 20:42:02 +0100

    fragments

--- src/app/courses/courses.component.ts
+++ src/app/courses/courses.component.ts
@@ -1,13 +1,31 @@
-import { Component, inject } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
 import { CourseService } from '../services/course.service';
 import { Course } from '../Models/course';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-courses',
   templateUrl: './courses.component.html',
   styleUrl: './courses.component.css'
 })
-export class CoursesComponent {
+export class CoursesComponent implements OnInit{
   coursesService = inject(CourseService);
-  AllCourses: Course[] = this.coursesService.courses;
+  AllCourses: Course[];
+  searchString: string;
+
+  activeRoute: ActivatedRoute = inject(ActivatedRoute);
+
+  ngOnInit(): void {
+      //this.searchString = this.activeRoute.snapshot.queryParams['search'];
+      //this.searchString = this.activeRoute.snapshot.queryParamMap.get('search');
+      this.activeRoute.queryParamMap.subscribe((data)=>{
+        this.searchString = data.get('search');
+
+        if(this.searchString === undefined || this.searchString === '' || this.searchString === null) {
+          this.AllCourses = this.coursesService.courses;
+        } else {
+          this.AllCourses = this.coursesService.courses.filter(c => c.title.toLowerCase().includes(this.searchString.toLowerCase()));
+        }
+      })     
+  }
 }
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit{
+  activeRoute: ActivatedRoute = inject(ActivatedRoute);
 
+  ngOnInit(): void {
+      this.activeRoute.fragment.subscribe((data)=>{
+        //console.log(data);
+        this.JumpToSection(data);
+      })
+  }
+
+  JumpToSection(section){
+    document.getElementById(section).scrollIntoView({behavior: "smooth", block: "start", inline: "nearest"});
+  }
 }
